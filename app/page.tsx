7da414600b6932c3018dce2d6bd37b88eed53db7
jimohsegun1 <jimohsegunj@gmail.com>
2024-09-19 15:26:36 +0100

    setup nextjs ecommerce app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// This Project file only contains the image not shadcn setup and other file setup
+// Watch the setup part of the video to fully understand the setup
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
