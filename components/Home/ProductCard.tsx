2133612d3335e5f398d4cd0a7911b563089e5e1c
jimohsegun1 <jimohsegunj@gmail.com>
2024-09-22 21:41:40 +0100

    products

---
+++ components/Home/ProductCard.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Product } from '@/typing'
+import React from 'react'
+
+type Props={
+product: Product
+}
+
+const ProductCard = ({product}: Props) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
