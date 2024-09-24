dd31b115c22c981be1ee3d82bf634179390796d3
jimohsegun1 <jimohsegunj@gmail.com>
2024-09-24 14:36:07 +0100

    integration

--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -6,6 +6,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import { useDispatch, useSelector } from 'react-redux'
+import { addItem } from '@/store/cartSlice'
+import { RootState } from '@/store/store'
 
 type Props = {
   product: Product
@@ -14,6 +17,16 @@ type Props = {
 const ProductCard = ({ product }: Props) => {
   const num = Math.round(product.rating.rate)
   const ratingArray = new Array(num).fill(0)
+
+  const items = useSelector((state: RootState) => state.cart.items)
+  console.log(items);
+  
+
+  const dispatch = useDispatch()
+
+  const addToCartHandler = (product: Product) => {
+    dispatch(addItem(product))
+  }
 
   return (
     <div className='p-4 border rounded'>
@@ -40,7 +53,7 @@ const ProductCard = ({ product }: Props) => {
         <p className='text-black text-lg font-bold opacity-80'>${product.price}</p>
       </div>
       <div className='mt-4 items-center space-x-2'>
-        <Button size={"icon"}>
+        <Button onClick={() => addToCartHandler(product)} size={"icon"}>
           <ShoppingBag size={18} />
         </Button>
         <Button size={"icon"} className='bg-red-500'>
