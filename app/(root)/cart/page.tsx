df7ce0b91d1ca8c4ac57605006f0b6d97e459b01
jimohsegun1 <jimohsegunj@gmail.com>
2024-09-25 16:55:02 +0100

    implementing payment

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { RootState } from '@/store/store'
+import { useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -16,6 +17,7 @@ const Cart = () => {
     const totalPriceWithVat = (+totalPrice + +vat).toFixed(2)
     console.log({ totalPrice, vat, totalPriceWithVat });
 
+    const { user } = useUser()
 
     return (
         <div className='mt-8 min-h-[60vh]'>
@@ -36,8 +38,54 @@ const Cart = () => {
                     <div className='rounded-lg shadow-md overflow-hidden xl:col-span-4'>
                         <h1 className='p-4 text-xl sm:text-2xl md:text-3xl font-bold text-white bg-blue-700'>Your Cart ({totalQuantity} Items)</h1>
                         {items.map((item) => {
-                            return <div></div>
+                            return (
+                                <div key={item.id}>
+                                    <div className='flex pb-6 mt-2 p-5 border-b-[1.5px] border-opacity-25 border-gray-700 items-center space-x-10'>
+                                        <div>
+                                            <Image src={item.image}
+                                                alt={item.title}
+                                                width={180}
+                                                height={180}
+                                            />
+                                        </div>
+                                        <div>
+                                            <h1 className='md:text-xl text-base font-bold text-black'>{item.title}</h1>
+                                            <h1 className='md:text-lg text-sm font-semibold'>Category:{item.category}</h1>
+                                            <h1 className='md:text-2xl text-lg font-bold text-blue-950'>${item.price}</h1>
+                                            <h1 className='md:text-lg text-sm font-semibold'>Quantity:{item.quantity}</h1>
+
+                                            <div className='flex items-center mt-4 space-x-2'>
+                                                <Button>Add More</Button>
+                                                <Button variant={"destructive"}>Remove</Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
                         })}
+                    </div>
+                    <div className='xl:col-span-2'>
+                        <div className='bg-indigo-950 sticky top-[25vh] p-6 rounded'>
+                            <h1 className='text-center mt-8 mb-8 text-white text-3xl font-semibold'>Summary</h1>
+                            <div className='w-full h-[1.2px] bg-white bg-opacity-20'></div>
+                            <div className='flex mt-4 text-xl uppercase font-semibold text-white items-center justify-between'>
+                                <span>Subtotal</span>
+                                <span>${totalPrice}</span>
+                            </div>
+                            <div className='flex mt-10 mb-10 text-xl uppercase font-semibold text-white items-center justify-between'>
+                                <span>VAT</span>
+                                <span>${vat}</span>
+                            </div>
+                            <div className='flex mt-10 mb-10 text-xl uppercase font-semibold text-white items-center justify-between'>
+                                <span>Shipping</span>
+                                <span>Free</span>
+                            </div>
+                            <div className='w-full h-[1.2px] bg-white bg-opacity-20'></div>
+                            <div className='flex mt-6 mb-6 text-xl uppercase font-semibold text-white items-center justify-between'>
+                                <span>Total</span>
+                                <span>${totalPriceWithVat}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
